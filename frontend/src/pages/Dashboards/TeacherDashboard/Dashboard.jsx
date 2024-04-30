27f0939b813b73508b113b2513486090be49d552
DimuthuAro <DimuthuAro@gmail.com>
2024-04-30 12:44:00 +0530

    Commit Dashboard Routes+

--- frontend/src/pages/Dashboards/TeacherDashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboards/TeacherDashboard/Dashboard.jsx
@@ -21,6 +21,7 @@ const Dashboard = () => {
                title="Students"
                description="Manage student profiles, including registration information and personal details. Update, suspend, or delete student accounts as necessary."
                buttonText="Manage"
+               to="/students"
             />
             <Dashcard
                imageSrc={papersImg}
