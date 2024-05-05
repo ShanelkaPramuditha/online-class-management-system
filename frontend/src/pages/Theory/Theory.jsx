184924a01b80951624a0892a8de249fcef7bb14f
Shanelka Pramuditha <99pramuditha@gmail.com>
2024-05-05 23:07:31 +0530

    chore: Add chat button to Theory page

--- frontend/src/pages/Theory/Theory.jsx
+++ frontend/src/pages/Theory/Theory.jsx
@@ -1,11 +1,24 @@
+import { useNavigate } from 'react-router-dom';
 import EnrollmentCard from '../../components/Card/EnrollmentCard';
 import CardImg_1 from '../../assets/images/card-image-01.png';
 
 const Theory = () => {
+   const navigate = useNavigate();
+
    return (
       <div className="body-content">
-         <div className="font-semibold text-3xl my-10 text-center">
-            Theory Classes
+         <div className="flex flex-col items-center">
+            <div className="font-semibold text-3xl my-10 text-center">
+               Theory Classes
+            </div>
+            {/* Chat Section */}
+            <div className="mr-auto">
+               <button
+                  className="bg-[#0057FF] hover:bg-[#000D85] text-[#FFFFFF] font-bold py-2 px-12 rounded-full m-9"
+                  onClick={() => navigate('/chat')}>
+                  Enter to the chat
+               </button>
+            </div>
          </div>
          <div className="grid grid-cols-4 gap-7  justify-items-center items-center">
             <div>
